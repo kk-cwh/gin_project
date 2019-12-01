9656f25f71464f518ef30c0101fb7c613e4d347b
zhangyake <zhangykvip@126.com>
2019-12-01 20:56:06 +0800

    add db

--- app/models/article.go
+++ app/models/article.go
@@ -2,19 +2,17 @@ package models
 
 import (
 	"fmt"
+
 	"github.com/jinzhu/gorm"
-	"time"
 )
 
 type Article struct {
 	Model
-	Name     string    `json:"name" gorm:"column(name);"`
-	Addr     string    `json:"addr" gorm:"column(addr);"`
-	Age      int       `json:"age" gorm:"column(age);"`
-	Birth    string    `json:"birth" gorm:"column(birth);"`
-	Sex      int       `json:"sex" gorm:"column(sex);"`
-	UpdateAt time.Time `json:"update_at" gorm:"column(update_at); description:"更新时间"`
-	CreateAt time.Time `json:"create_at" gorm:"column(create_at);type(datetime)" description:"创建时间"`
+	Name  string `json:"name" gorm:"column(name);"`
+	Addr  string `json:"addr" gorm:"column(addr);"`
+	Age   int    `json:"age" gorm:"column(age);"`
+	Birth string `json:"birth" gorm:"column(birth);"`
+	Sex   int    `json:"sex" gorm:"column(sex);"`
 }
 
 // GetArticles gets a list of articles based on paging constraints
